6de9550e81ca20d193cd393dd53a0d6dca1f5107
JerosCalmera <136751073+JerosCalmera@users.noreply.github.com>
2024-04-17 06:09:20 +0100

    Update GameBoard.tsx

--- src/containers/GameBoard.tsx
+++ src/containers/GameBoard.tsx
@@ -9,7 +9,7 @@ import LoadingSplash from "../components/LoadingSplash";
 function GameBoard() {
     const BASE_URL = "https://solar-fury-backend-v1-a5761b56a343.herokuapp.com/"
 
-    const [stompClient, setStompClient] = useState<Stomp.Client>(Stomp.over(new SockJS(`${BASE_URL}:8081/game`)));
+    const [stompClient, setStompClient] = useState<Stomp.Client>(Stomp.over(new SockJS(`${BASE_URL}/game`)));
     const [serverStatus, setServerStatus] = useState(false)
     const [attemptReconnect, setAttemptReconnect] = useState(0)
     const [serverMessageLog, serverSetMessageLog] = useState("")
