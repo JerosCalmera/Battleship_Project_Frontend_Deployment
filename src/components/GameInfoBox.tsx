8303ee71a63391b9b08c17f48e86bfc3620f3463
Jack Chaplin <136751073+JerosCalmera@users.noreply.github.com>
2024-05-07 13:39:35 +0100

    visual update

--- src/components/GameInfoBox.tsx
+++ src/components/GameInfoBox.tsx
@@ -28,7 +28,7 @@ const GameInfoBox: React.FC<Props> = ({enemyShipsRemaining, player1Data, turnNum
             <div className="gameInfoOuter">
                 <div className="gameInfo">
                 <h4>Turn: ({turnNumber}) {turn.includes("Computer") ? "Computer" : turn}</h4>
-                    {shipInfo.length === 60 && readyStatus != "Ready" ? <h4>Waiting....</h4> : null}
+                    {shipInfo.length === 60 && readyStatus != "Ready" ? <h4>Waiting for all players to be ready</h4> : null}
                     {shipInfo.length > 1 && readyStatus === "Ready" ? <h4>{gameInfo}</h4> : null}
                     {shipInfo.length > 1 && readyStatus === "Ready" ? <h4>Enemy Ships remaining: {enemyShipsRemaining}</h4> : null}
                     {shipInfo.length === 60 && readyStatus === "Not Ready" ? <button onClick={handleConfirmReady} className="button">Confirm Ready</button> : null}
