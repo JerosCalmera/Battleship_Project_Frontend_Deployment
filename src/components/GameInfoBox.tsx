ab399d4e255417858871537696327efc8d4a909b
Jack Chaplin <136751073+JerosCalmera@users.noreply.github.com>
2024-05-07 12:35:22 +0100

    Update GameInfoBox.tsx

--- src/components/GameInfoBox.tsx
+++ src/components/GameInfoBox.tsx
@@ -23,10 +23,10 @@ const GameInfoBox: React.FC<Props> = ({enemyShipsRemaining, player1Data, turnNum
                 <div className="gameInfo">
                 <h4>Turn: ({turnNumber}) {turn.includes("Computer") ? "Computer" : turn}</h4>
                     <h4>{gameInfo}</h4>
+                    <h4>Enemy Ships remaining: {enemyShipsRemaining}</h4>
                     {shipInfo.length === 60 && matchStart.length > 1 ? <button onClick={matchBegin} className="button">Confirm Ready</button> : null}
                     {shipInfo.length > 1 && shipInfo.length < 60 ? <h4>Placing ships...</h4> : null}
                     {shipInfo.length < 1 && matchStart.length > 2 ? <button onClick={randomPlacement} className="button">Random Ship Placement</button> : null}
-                    <h4>Enemy Ships remaining: {enemyShipsRemaining}</h4>
                 </div>
             </div> : null}
         </>
