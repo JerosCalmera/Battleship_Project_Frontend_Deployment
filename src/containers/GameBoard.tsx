bcabc89699557e2d4324d70e9726ec7823f0346f
Jack Chaplin <136751073+JerosCalmera@users.noreply.github.com>
2024-05-28 00:39:45 +0100

    Update GameBoard.tsx

--- src/containers/GameBoard.tsx
+++ src/containers/GameBoard.tsx
@@ -375,7 +375,7 @@ function GameBoard() {
     const hiddenParse = (message: any) => {
         if (message.includes(roomNumberSave.current)) {
         setHidden(message)}
-        if (message.includes(roomNumberSave.current) && "Player left") {
+        if (message.includes(roomNumberSave.current) && "Player left" && !playerNameSave.current) {
             setPlayerLeft(0)}
     }
 
