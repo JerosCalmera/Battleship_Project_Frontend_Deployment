42d9c3a84b4354ea56d2a7e5aa790cb26c7ab10b
Jack Chaplin <136751073+JerosCalmera@users.noreply.github.com>
2024-05-28 12:59:53 +0100

    Update GameBoard.tsx

--- src/containers/GameBoard.tsx
+++ src/containers/GameBoard.tsx
@@ -217,7 +217,7 @@ function GameBoard() {
         if (loading == true) {
             setLoading(false)
         }
-    }, [hidden, serverMessageLog])
+    }, [hidden, chat, serverMessageLog])
 
     // Logic for checking who a missed shot belongs to for display
     useEffect(() => {
