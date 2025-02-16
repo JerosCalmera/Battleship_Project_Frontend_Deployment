626ea9553d58b82b643a532f77dfbf34c0c22cb0
Jeros <136751073+JerosCalmera@users.noreply.github.com>
2025-02-16 07:23:28 +0000

    Update GameInfoBox.tsx

--- src/components/GameInfoBox.tsx
+++ src/components/GameInfoBox.tsx
@@ -34,9 +34,9 @@ const GameInfoBox: React.FC<Props> = ({ friendlyShipsRemaining, enemyShipsRemain
                         {shipInfo.length < 60 && readyStatus != "Ready" ? <h4>Waiting on ship placement</h4> : null}
                         {shipInfo.length === 60 && readyStatus != "Ready" ? <h4>Waiting for all players to be ready</h4> : null}
                         <div className="centre">
-                            {shipInfo.length === 60 && readyStatus === "Not Ready" ? <><br /> <br /> <button onClick={handleConfirmReady} className="button">Ready</button></> : null}
+                            {shipInfo.length === 60 && readyStatus === "Not Ready" ? <><br /> <button onClick={handleConfirmReady} className="button">Ready</button></> : null}
                             {shipInfo.length > 1 && shipInfo.length < 60 ? <h4>Placing ships...</h4> : null}
-                            {shipInfo.length < 1 && readyStatus === "Not Ready" ? <><br /> <br /> <button onClick={randomPlacement} className="button">Random Ship Placement</button></> : null}
+                            {shipInfo.length < 1 && readyStatus === "Not Ready" ? <><br /> <button onClick={randomPlacement} className="button">Random Ship Placement</button></> : null}
                         </div>
                         {shipInfo.length > 1 && readyStatus === "Ready" ? <h4>Enemy Ships remaining: {enemyShipsRemaining}</h4> : null}
                         {shipInfo.length > 1 && readyStatus === "Ready" ? <h4>Friendly Ships remaining: {friendlyShipsRemaining}</h4> : null}
